6c9ecfdae420d98a7dc4de831904f2d191224cf3
gmalho <gaurav.malhotra@nike.com>
2019-04-06 11:43:06 +0200

    CBR

---
+++ src/main/java/com/gonnect/sb/camel/routes/cbr/CbrRoute.java
@@ -0,0 +1,29 @@
+package com.gonnect.sb.camel.routes.cbr;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component("cbr")
+public class CbrRoute extends RouteBuilder {
+
+    @Value("classpath:input")
+    private Resource resource;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("file:" + resource.getURI().getPath()).
+                choice()
+                .when().jsonpath("$[?(@.country == 'IND')]")
+                    .to("stream:out")
+                .when().jsonpath("$[?(@.country == 'NL')]")
+                    .to("stream:out")
+                .when().jsonpath("$[?(@.country == 'UK')]")
+                    .to("stream:out")
+                .when().jsonpath("$[?(@.country == 'US')]")
+                    .to("stream:out")
+                .end();
+    }
+}
